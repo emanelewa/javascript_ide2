185ab2b7d2a753938b9aabd4f19860250200975b
emanelewa <eman.elewa@hotmail.com>
2020-12-25 15:02:15 +0200

    quiz 1

---
+++ quez1/even & odd/js.js
@@ -0,0 +1,7 @@
+let num = Number(prompt("Enter your number"));
+if(num % 2 == 0){
+    document.write("even")
+}else{
+    document.write("odd")
+}
+
---
+++ quez1/your age/js.js
@@ -0,0 +1,7 @@
+//task 2
+let yourAge = Number(prompt("Enter your Age"));
+if(yourAge >= 18){
+    alert("sign up")
+}else {
+    alert("sorry you can't sign up")
+}
